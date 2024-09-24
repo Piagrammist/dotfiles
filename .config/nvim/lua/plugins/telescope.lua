fb5c03545e62d058013d89d2761d930cd4ccdaef
Piagrammist <Piagrammist@gmail.com>
2024-09-24 17:52:30 +0330

    Nvim plugins: convert `config` to `opts`

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -2,7 +2,6 @@ return {
     "nvim-telescope/telescope.nvim",
     dependencies = { "nvim-lua/plenary.nvim" },
     config = function()
-        --require("telescope").setup()
         local telescope = require("telescope.builtin")
         vim.keymap.set("n", "<C-p>", telescope.find_files, {})
         vim.keymap.set("n", "<leader>fg", telescope.live_grep, {})
--- .config/nvim/lua/plugins/tokyodark.lua
+++ .config/nvim/lua/plugins/tokyodark.lua
@@ -2,7 +2,6 @@ return {
     "tiagovla/tokyodark.nvim",
     priority = 1000,
     config = function()
-        --require("tokyodark").setup()
         vim.cmd [[ colorscheme tokyodark ]]
     end,
 }
