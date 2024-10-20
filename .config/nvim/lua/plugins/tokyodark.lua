7716ce2d8a4d611fc8c14bdc592b1669e978e134
Piagrammist <Piagrammist@gmail.com>
2024-10-20 20:48:00 +0330

    Disable `tokyodark.nvim` italics

--- .config/nvim/lua/plugins/tokyodark.lua
+++ .config/nvim/lua/plugins/tokyodark.lua
@@ -3,6 +3,13 @@ return {
     priority = 1000,
     opts = {
         transparent_background = true,
+        styles = {
+            comments = { italic = false },
+            keywords = { italic = false },
+            identifiers = { italic = false },
+            functions = {},
+            variables = {},
+        },
     },
     config = function(_, opts)
         require("tokyodark").setup(opts)
