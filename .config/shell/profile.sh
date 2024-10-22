22ac8fe4ee86806fde7b74af9e34e8419940e776
Piagrammist <Piagrammist@gmail.com>
2024-10-23 00:15:50 +0330

    Add dmenu scripts to `PATH`

--- .config/shell/profile.sh
+++ .config/shell/profile.sh
@@ -1,6 +1,7 @@
-[ -d "$HOME/.bin"       ] && PATH="$HOME/.bin:$PATH"
-[ -d "$HOME/.local/bin" ] && PATH="$HOME/.local/bin:$PATH"
-[ -d "$HOME/.scripts"   ] && PATH="$PATH:$HOME/.scripts"
+[ -d "$HOME/.bin"           ] && PATH="$HOME/.bin:$PATH"
+[ -d "$HOME/.local/bin"     ] && PATH="$HOME/.local/bin:$PATH"
+[ -d "$HOME/.scripts"       ] && PATH="$PATH:$HOME/.scripts"
+[ -d "$HOME/.scripts/dmenu" ] && PATH="$PATH:$HOME/.scripts/dmenu"
 export PATH
 
 if [ -z "$DISPLAY" ] && [ $XDG_VTNR -eq 1 ]; then
