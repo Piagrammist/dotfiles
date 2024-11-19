affab91d7956a00131e3baa46b985546cfb91699
Piagrammist <Piagrammist@gmail.com>
2024-11-19 17:52:09 +0330

    Add Cargo bin folder to path

--- .config/shell/profile.sh
+++ .config/shell/profile.sh
@@ -2,6 +2,7 @@
 [ -d "$HOME/.local/bin"     ] && PATH="$HOME/.local/bin:$PATH"
 [ -d "$HOME/.scripts"       ] && PATH="$PATH:$HOME/.scripts"
 [ -d "$HOME/.scripts/dmenu" ] && PATH="$PATH:$HOME/.scripts/dmenu"
+[ -d "$HOME/.cargo/bin"     ] && PATH="$PATH:$HOME/.cargo/bin"
 export PATH
 
 if [ -z "$DISPLAY" ] && [ $XDG_VTNR -eq 1 ]; then
